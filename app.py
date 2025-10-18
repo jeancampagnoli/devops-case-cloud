eb825b1ab2216a4a3113d52865fb2b69a2d6b106
jeancampagnoli <campagnoli.jean@gmail.com>
2025-10-18 15:14:51 -0300

    fix: modifid integration

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    # Uma variável de ambiente para mostrar a atualização
+    version = os.environ.get('APP_VERSION', '1.0')
+    return f"<h1>Olá! Bem-vindo ao Case DevOps!</h1><p>Versão da Aplicação: {version}</p>"
+
+@app.route('/health')
+def health_check():
+    # Rota para o monitoramento verificar se a app está no ar
+    return "OK", 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
